248fa065ed2fd04084d5c7358f527bec9383a19c
Jared Schutt <jaredschutt22@gmail.com>
2019-10-19 15:13:22 -0400

    chore: removed all python files, switching project to a React, NodeJS, ExpressJS app.

--- labels.py
+++
@@ -1,6 +0,0 @@
-# labels.py
-APPLICATION_NAME = "Go With The Flow"
-NAVIGATION = "Navigation"
-RISING_SEA_LEVELS = "Rising Sea Levels"
-THERMAL_EXPANSION = "Thermal Expansion"
-ICE_MELTING = "Ice Melting"
